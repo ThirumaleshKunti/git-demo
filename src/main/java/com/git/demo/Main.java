015be5f790af34d2e371d2f3e96c2c17ad557c3f
Thirumalesh <kunti.thirumalesh@gmail.com>
2023-03-08 14:47:30 +0530

    added git demo to local repository

---
+++ src/main/java/com/git/demo/Main.java
@@ -0,0 +1,13 @@
+package com.git.demo;
+
+public class Main {
+
+	public void getMessage(String message) {
+		System.out.println("Message : " + message);
+	}
+
+	public static void main(String[] args) {
+          System.out.println("inside main method...");
+	}
+
+}
